41643e134c3a4ea925b343cb53161305e139e5be
kameha1011 <omarcitofc2001@gmail.com>
2023-02-11 20:21:31 -0300

    added logic to alternate visibility of artist names and icos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,25 @@
-console.log('hi!!!!!!!!!!');
 const nav = document.querySelector('nav');
+const allIcons = document.querySelectorAll('.social-media-icons');
+const artistNames = document.querySelectorAll('.artist-name');
+const artistImgs = document.querySelectorAll('.artist-picture');
+// change nav bgcolor
 window.addEventListener('scroll', () => {
-    console.log(window.scrollY);
     (window.scrollY >= 100)? nav.setAttribute('id', 'navbar') : nav.removeAttribute('id');
 })
+// alternate visibility for artist names and icos
+for (let i = 0; i < allIcons.length; i++) {
+    const icons = allIcons[i];
+    const artistName = artistNames[i];
+    const artistImg = artistImgs[i];
+    
+    artistImg.addEventListener('mouseover', () => {
+        artistName.classList.add('show-block');
+        icons.classList.add('show-block');
+        console.log('ejele');
+    });
+    artistImg.addEventListener('mouseleave', () => {
+        artistName.classList.remove('show-block');
+        icons.classList.remove('show-block');
+        console.log('chao');
+    })
+}
