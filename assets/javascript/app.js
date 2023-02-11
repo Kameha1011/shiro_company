78e79843645bf69f3e4e7927c76624ab4048291f
kameha1011 <omarcitofc2001@gmail.com>
2023-02-11 18:36:23 -0300

    added logic to change nav bgcolor when scrolling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,6 @@
-console.log('hi!!!!!!!!!!')
+console.log('hi!!!!!!!!!!');
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    (window.scrollY >= 100)? nav.setAttribute('id', 'navbar') : nav.removeAttribute('id');
+})
