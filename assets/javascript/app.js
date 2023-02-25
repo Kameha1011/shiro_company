bc580fa01fc9f14627315ef5605beffa5749b448
Kameha1011 <omarcitofc2001@gmail.com>
2023-02-25 19:39:31 -0300

    added animations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,3 +23,17 @@ for (let i = 0; i < allIcons.length; i++) {
         console.log('chao');
     })
 }
+// animation
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
