1a3f8bac067dc0690bc08d6ffb5cc9f067234dab
Kameha1011 <omarcitofc2001@gmail.com>
2023-03-04 21:53:58 -0300

    emailjs feature added

---
+++ assets/javascript/emailjs.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('form-button');
+
+document.getElementById('contact-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_mm1au82';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      window.location.replace('http://127.0.0.1:5500/thanks.html')
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
