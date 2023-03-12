3c484d8ebd7189b2d28ddfddc9d3aa61d68c0813
AnthonyLilo <ammen0203@gmail.com>
2023-03-12 18:44:03 -0300

    Rutas amigables

--- assets/javascript/emailjs.js
+++ assets/javascript/emailjs.js
@@ -12,11 +12,11 @@ document.getElementById('contact-form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
-      // window.location.replace('http://127.0.0.1:5500/thanks.html');
-      window.location.replace('https://shirocompany.com/thanks.html');
+      // window.location.replace('http://127.0.0.1:5500/thanks');
+      window.location.replace('https://shirocompany.com/thanks');
     }, (err) => {
       btn.value = 'Send Email';
       // alert(JSON.stringify(err));
-      window.location.replace('https://shirocompany.com/error.html');
+      window.location.replace('https://shirocompany.com/error');
     });
 });
